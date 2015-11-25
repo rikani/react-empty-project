5f1e310753dc1ab9bf2233e7d9270071a7472a4c
rikani <rikani86@gmail.com>
2015-11-25 20:07:43 +0300

    Add TodoList Example with Flux

--- www/f/src/app.js
+++ www/f/src/app.js
@@ -4,5 +4,7 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 
 import MyComponent from './components/MyComponent/MyComponent.react';
+import TodoApp from './components/TodoApp.react';
 
 ReactDOM.render(<MyComponent name="MyComponent" />, document.getElementById('react-view'));
+ReactDOM.render(<TodoApp />, document.getElementById('todoapp'));
